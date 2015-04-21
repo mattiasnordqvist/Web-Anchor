419aad2b49745a7ac08eab107519a1c392041a58
Mattias Nordqvist <mattias.nordqvist@gmail.com>
2015-04-21 19:04:28 +0200

    #7 Adopting semver
    and adding a ProductAssemblyInfo

---
+++ ProductAssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyFileVersion("0.0.1")]
+[assembly: AssemblyProduct("WebAnchor")]
+[assembly: AssemblyCompany("Spinit AB, Mattias Nordqvist, Carl Berg and contributors")]
+[assembly: AssemblyCopyright("Copyright © Spinit AB, Mattias Nordqvist, Carl Berg and contributors 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
