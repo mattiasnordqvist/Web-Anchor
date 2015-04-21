f5efdeecc172d287ebd24aa5f73e28c0ae804017
Mattias Nordqvist <mattias.nordqvist@gmail.com>
2015-04-21 19:32:52 +0200

    stylecop

--- WebAnchor/Properties/AssemblyInfo.cs
+++ WebAnchor/Properties/AssemblyInfo.cs
@@ -5,9 +5,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("A type-safe .NET-library for consuming web APIs")]
 [assembly: AssemblyConfiguration("")]
 
-
 [assembly: ComVisible(false)]
 
 [assembly: Guid("7407acbc-1d32-415f-a056-b1f9f7e1e17b")]
-
-
