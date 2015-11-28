b554ed7b27e0c0e8c4f7621b7d2c5ef8f2547bd3
Mattias Nordqvist <mattias.nordqvist@gmail.com>
2015-11-28 12:41:46 +0100

    IHttpClient implements IDisposable

--- WebAnchor/IHttpClient.cs
+++ WebAnchor/IHttpClient.cs
@@ -1,11 +1,11 @@
+using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace WebAnchor
 {
-    public interface IHttpClient
+    public interface IHttpClient : IDisposable
     {
         Task<HttpResponseMessage> SendAsync(HttpRequestMessage request);
-        void Dispose();
     }
 }
