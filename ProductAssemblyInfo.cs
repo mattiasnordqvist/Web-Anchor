e42e7c96ba41f0881c6d15288a735eff8bc14e4c
Mattias Nordqvist <mattias.nordqvist@gmail.com>
2019-04-09 22:03:50 +0200

    updated analyzer package

--- ProductAssemblyInfo.cs
+++ ProductAssemblyInfo.cs
@@ -1,11 +1,11 @@
 using System.Reflection;
 [assembly: AssemblyProduct("WebAnchor")]
 [assembly: AssemblyCompany("Spinit AB, Mattias Nordqvist, Carl Berg and contributors")]
-[assembly: AssemblyCopyright("Copyright © Spinit AB, Mattias Nordqvist, Carl Berg and contributors 2018")]
+[assembly: AssemblyCopyright("Copyright © Spinit AB, Mattias Nordqvist, Carl Berg and contributors 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 /* File versions are set by CI build */
-[assembly: AssemblyVersion("6.5.2")]
-[assembly: AssemblyFileVersion("6.5.2")]
-[assembly: AssemblyInformationalVersion("6.5.2")]
+[assembly: AssemblyVersion("6.5.3")]
+[assembly: AssemblyFileVersion("6.5.3")]
+[assembly: AssemblyInformationalVersion("6.5.3")]
