0c4c4a122dbfcc39997c2851672255b7d6998170
Mattias Nordqvist <mattias.nordqvist@gmail.com>
2020-03-09 18:27:55 +0100

    version bump

--- ProductAssemblyInfo.cs
+++ ProductAssemblyInfo.cs
@@ -6,6 +6,6 @@ using System.Reflection;
 [assembly: AssemblyCulture("")]
 
 /* File versions are set by CI build */
-[assembly: AssemblyVersion("6.6.0")]
-[assembly: AssemblyFileVersion("6.6.0")]
-[assembly: AssemblyInformationalVersion("6.6.0")]
+[assembly: AssemblyVersion("6.6.1")]
+[assembly: AssemblyFileVersion("6.6.1")]
+[assembly: AssemblyInformationalVersion("6.6.1")]
