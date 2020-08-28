ad1541e11c18b67c7141fc2382139c1d7eedcb5b
olpej <olle.pejstrup@spinit.se>
2020-08-28 13:33:38 +0200

    Added xml serializer

---
+++ WebAnchor/Attributes/Content/XmlAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using WebAnchor.Attributes.Content;
+using WebAnchor.RequestFactory.Transformation;
+
+namespace WebAnchor.RequestFactory.Serialization
+{
+    [AttributeUsage(AttributeTargets.Parameter)]
+    public class XmlWebAnchorAttribute : ParameterTransformerAttribute
+    {
+        public override void Apply(Parameter parameter, RequestTransformContext requestTransformContext)
+        {
+            requestTransformContext.ContentSerializer = new XmlContentSerializer();
+        }
+        public override void ValidateApi(Type type, MethodInfo method, ParameterInfo parameter)
+        {
+            if (!parameter.GetCustomAttributes(typeof(ContentAttribute), false).Any())
+            {
+                throw new WebAnchorException($"The attribute {nameof(XmlWebAnchorAttribute)} cannot be used on parameter {parameter.Name} because it does not have a {nameof(ContentAttribute)}");
+            }
+        }
+    }
+}
---
+++ WebAnchor/RequestFactory/Serialization/XmlContentSerializer.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace WebAnchor.RequestFactory.Serialization
+{
+    internal class XmlContentSerializer : IContentSerializer
+    {
+
+        public virtual HttpContent Serialize(object value, Parameter content)
+        {
+            if (content == null)
+            {
+                return null;
+            }
+
+            var s1 = new XmlSerializer(content.SourceParameterInfo.ParameterType);
+            var builder = new StringBuilder();
+            var xmlws = new XmlWriterSettings { OmitXmlDeclaration = true, Indent = false, NewLineOnAttributes = false, Encoding = Encoding.UTF8 };
+            using (var writer = XmlWriter.Create(builder, xmlws))
+            {
+                s1.Serialize(writer, value);
+            }
+            string xml = builder.ToString();
+
+            return new StringContent(xml, Encoding.UTF8, "application/xml");
+
+        }
+    }
+}
